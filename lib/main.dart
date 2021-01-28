5e6483d422c65018c3f6b1b80245ae4d2dab20af
Surhid Amatya <surhid.amatya@gmail.com>
2021-01-28 14:54:43 +0545

    add provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weatherlyapp/viewmodels/city_view_model.dart';
+import 'package:weatherlyapp/viewmodels/forecast_view_model.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider<CityEntryViewModel>(
+        create: (_) => CityEntryViewModel()),
+    ChangeNotifierProvider<ForecastViewModel>(
+        create: (_) => ForecastViewModel()),
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weatherly',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
